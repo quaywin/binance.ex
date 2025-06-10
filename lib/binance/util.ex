ba0038409493ff5f02165f73d26ab6f8bbd52e0a
Thang Nguyen <quaywin@gmail.com>
2025-06-10 16:49:31 +0700

    Improve RSA signing by cleaning key and handling errors

--- lib/binance/util.ex
+++ lib/binance/util.ex
@@ -21,11 +21,20 @@ defmodule Binance.Util do
   Sign a given string using given key using secret key
   """
   def sign_content(key, content, key_type) when key_type == "rsa" do
-    IO.inspect(key)
+    # Clean the key format
+    cleaned_key = String.replace(key, "\\n", "\n")
 
-    {:ok, rsa_priv_key} = ExPublicKey.loads(key)
-    {:ok, signature} = ExPublicKey.sign(content, rsa_priv_key)
-    "#{Base.encode64(signature)}" |> URI.encode_www_form()
+    case ExPublicKey.loads(cleaned_key) do
+      {:ok, rsa_priv_key} ->
+        case ExPublicKey.sign(content, rsa_priv_key) do
+          {:ok, signature} ->
+            "#{Base.encode64(signature)}" |> URI.encode_www_form()
+          {:error, reason} ->
+            raise "Failed to sign: #{reason}"
+        end
+      {:error, reason} ->
+        raise "Invalid RSA key: #{reason}"
+    end
   end
 
   def sign_content(key, content, _key_type) do
